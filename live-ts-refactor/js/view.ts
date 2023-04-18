fbefe438e2fa8e7c002e43a8e47ee4084f966f75
jstevenson02 <jacobryanstevenson@gmail.com>
2023-04-18 16:36:10 -0500

    View strongly typed

--- live-ts-refactor/js/view.ts
+++ live-ts-refactor/js/view.ts
@@ -1,4 +1,4 @@
-import type { Game } from "./types";
+import type { Move, Player } from "./types";
 import type Store from "./store";
 
 export default class View {
@@ -54,26 +54,23 @@ export default class View {
 
   // Register all event listeners
 
-  bindGameResetEvent(handler) {
+  bindGameResetEvent(handler: EventListener) {
     this.$.resetBtn.addEventListener("click", handler);
     this.$.modalBtn.addEventListener("click", handler);
   }
 
-  bindNewRoundEvent(handler) {
+  bindNewRoundEvent(handler: EventListener) {
     this.$.newRoundBtn.addEventListener("click", handler);
   }
-  bindPlayerMoveEvent(handler) {
+  bindPlayerMoveEvent(handler: EventListener) {
     this.#delegate(this.$.grid, '[data-id="square"]', "click", handler);
-    // this.$$.squares.forEach((square) => {
-    //   square.addEventListener("click", () => handler(square));
-    // });
   }
 
   // DOM Helper methods
-  #updateScoreboard(p1Wins, p2Wins, ties) {
-    this.$.p1Wins.innerText = `${p1Wins} wins`;
-    this.$.p2Wins.innerText = `${p2Wins} wins`;
-    this.$.ties.innerText = `${ties}`;
+  #updateScoreboard(p1Wins: number, p2Wins: number, ties: number) {
+    this.$.p1Wins.textContent = `${p1Wins} wins`;
+    this.$.p2Wins.textContent = `${p2Wins} wins`;
+    this.$.ties.textContent = `${ties}`;
   }
 
   #closeAll() {
@@ -81,9 +78,9 @@ export default class View {
     this.#closeMenu();
   }
 
-  #openModal(message) {
+  #openModal(message: string) {
     this.$.modal.classList.remove("hidden");
-    this.$.modalText.innerText = message;
+    this.$.modalText.textContent = message;
   }
 
   #clearMoves() {
@@ -92,7 +89,7 @@ export default class View {
     });
   }
 
-  #initializeMoves(moves) {
+  #initializeMoves(moves: Move[]) {
     this.$$.squares.forEach((square) => {
       const existingMove = moves.find((move) => move.squareId === +square.id);
 
@@ -110,7 +107,10 @@ export default class View {
     this.$.menuItems.classList.add("hidden");
     this.$.menuBtn.classList.remove("border");
 
-    const icon = this.$.menuBtn.querySelector("i");
+    // const icon = this.$.menuBtn.querySelector("i");
+
+    // Type narrowing, better debugging
+    const icon = this.#qs("i", this.$.menuBtn);
 
     icon.classList.add("fa-chevron-down");
     icon.classList.remove("fa-chevron-up");
@@ -120,20 +120,20 @@ export default class View {
     this.$.menuItems.classList.toggle("hidden");
     this.$.menuBtn.classList.toggle("border");
 
-    const icon = this.$.menuBtn.querySelector("i");
+    const icon = this.#qs("i", this.$.menuBtn);
 
     icon.classList.toggle("fa-chevron-down");
     icon.classList.toggle("fa-chevron-up");
   }
 
-  #handlePlayerMove(squareEl, player) {
+  #handlePlayerMove(squareEl: Element, player: Player) {
     const icon = document.createElement("i");
     icon.classList.add("fa-solid", player.iconClass, player.colorClass);
     squareEl.replaceChildren(icon);
   }
 
   // Player = 1 | 2
-  #setTurnIndicator(player) {
+  #setTurnIndicator(player: Player) {
     const icon = document.createElement("i");
     const label = document.createElement("p");
 
@@ -167,9 +167,18 @@ export default class View {
     return el;
   }
 
-  #delegate(el, selector, eventKey, handler) {
+  #delegate(
+    el: Element,
+    selector: string,
+    eventKey: string,
+    handler: (el: Element) => void
+  ) {
     el.addEventListener(eventKey, (event) => {
-      if (event.target.matches(selector)) {
+      if (!(event.target instanceof Element)) {
+        throw new Error("Event target not found");
+      }
+
+      if (event.target?.matches(selector)) {
         handler(event.target);
       }
     });

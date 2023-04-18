199422c04bdc6a42f721c33cf4a0c5dd4b613550
jstevenson02 <jacobryanstevenson@gmail.com>
2023-04-18 15:15:20 -0500

    Setup tsconfig, organized typesafe folder structur

---
+++ live-ts-refactor/src/js/app.ts
@@ -0,0 +1,58 @@
+import Store from "./store";
+import View from "./view";
+
+const players = [
+  {
+    id: 1,
+    name: "Player 1",
+    iconClass: "fa-x",
+    colorClass: "yellow",
+  },
+  {
+    id: 2,
+    name: "Player 2",
+    iconClass: "fa-o",
+    colorClass: "turquoise",
+  },
+];
+
+function init() {
+  const view = new View();
+  const store = new Store("live-t3-key", players);
+
+  // Current tab state changes
+  store.addEventListener("statechange", () => {
+    view.render(store.game, store.stats);
+  });
+
+  // Different tab state changes
+  window.addEventListener("storage", () => {
+    view.render(store.game, store.stats);
+  });
+
+  // First load of the document
+  view.render(store.game, store.stats);
+
+  view.bindGameResetEvent((event) => {
+    store.reset();
+  });
+
+  view.bindNewRoundEvent((event) => {
+    store.newRound();
+  });
+
+  view.bindPlayerMoveEvent((square) => {
+    const existingMove = store.game.currentGameMoves.find(
+      (move) => move.squareId === +square.id
+    );
+
+    if (existingMove) {
+      return;
+    }
+
+    // Advance to the next state by pushing a move to the move array
+    store.playerMove(+square.id);
+  });
+}
+
+window.addEventListener("load", init);

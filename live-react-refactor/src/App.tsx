34b4ebb7664a6ff9ab67d51eff9ae2c969999c47
jstevenson02 <jacobryanstevenson@gmail.com>
2023-04-20 15:36:01 -0500

    LocalStorage, gamestate tracked, turnindicator

--- live-react-refactor/src/App.tsx
+++ live-react-refactor/src/App.tsx
@@ -3,8 +3,9 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Menu from "./components/Menu";
 import Modal from "./components/Modal";
-import { GameState, Player } from "./types";
+import { Game, GameState, Player } from "./types";
 import classNames from "classnames";
+import { useLocalStorage } from "./useLocalStorage";
 
 const players: Player[] = [
   {
@@ -78,7 +79,7 @@ function deriveStats(state: GameState) {
 }
 
 export default function App() {
-  const [state, setState] = useState({
+  const [state, setState] = useLocalStorage<GameState>("game-state-key", {
     currentGameMoves: [],
     history: {
       currentRoundGames: [],
@@ -89,20 +90,59 @@ export default function App() {
   const game = deriveGame(state);
   const stats = deriveStats(state);
 
-  function handlePlayerMove(squareId: number, player: Player) {}
+  function resetGame(isNewRound: boolean) {
+    setState((prev) => {
+      const stateClone = structuredClone(prev);
+
+      const { status, moves } = game;
+
+      if (status.isComplete) {
+        stateClone.history.currentRoundGames.push({
+          moves,
+          status,
+        });
+      }
+
+      stateClone.currentGameMoves = [];
+
+      if (isNewRound) {
+        stateClone.history.allGames.push(
+          ...stateClone.history.currentRoundGames
+        );
+        stateClone.history.currentRoundGames = [];
+      }
+
+      return stateClone;
+    });
+  }
+
+  function handlePlayerMove(squareId: number, player: Player) {
+    setState((prev) => {
+      const stateClone = structuredClone(prev);
+
+      stateClone.currentGameMoves.push({
+        squareId,
+        player,
+      });
+
+      return stateClone;
+    });
+  }
 
   return (
     <>
       <main>
-        <div className="grid" data-id="grid">
-          <div className="turn" data-id="turn">
-            <i className="fa-solid fa-x yellow"></i>
-            <p className="yellow">Player 1, you're up!</p>
+        <div className="grid">
+          <div className={classNames("turn", game.currentPlayer.colorClass)}>
+            <i
+              className={classNames("fa-solid", game.currentPlayer.iconClass)}
+            ></i>
+            <p>{game.currentPlayer.name}, you're up!</p>
           </div>
 
           <Menu
-            onNewRound={() => console.log()}
-            onReset={() => console.log()}
+            onNewRound={() => resetGame(false)}
+            onReset={() => resetGame(true)}
           />
 
           {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((squareId) => {
@@ -161,6 +201,7 @@ export default function App() {
           message={
             game.status.winner ? `${game.status.winner.name} wins!` : "Tie!"
           }
+          onClick={() => resetGame(false)}
         />
       )}
     </>

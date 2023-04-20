f40c493ad56f8b5d7679714789e22078f3dd50c3
jstevenson02 <jacobryanstevenson@gmail.com>
2023-04-20 15:38:58 -0500

    Cleaned up files

--- live-react-refactor/src/App.tsx
+++ live-react-refactor/src/App.tsx
@@ -1,82 +1,11 @@
-import { useState } from "react";
 import "./App.css";
 import Footer from "./components/Footer";
 import Menu from "./components/Menu";
 import Modal from "./components/Modal";
-import { Game, GameState, Player } from "./types";
+import { GameState, Player } from "./types";
 import classNames from "classnames";
 import { useLocalStorage } from "./useLocalStorage";
-
-const players: Player[] = [
-  {
-    id: 1,
-    name: "Player 1",
-    iconClass: "fa-x",
-    colorClass: "yellow",
-  },
-  {
-    id: 2,
-    name: "Player 2",
-    iconClass: "fa-o",
-    colorClass: "turquoise",
-  },
-];
-
-function deriveGame(state: GameState) {
-  const currentPlayer = players[state.currentGameMoves.length % 2];
-
-  const winningPatterns = [
-    [1, 2, 3],
-    [1, 5, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 5, 7],
-    [3, 6, 9],
-    [4, 5, 6],
-    [7, 8, 9],
-  ];
-
-  let winner = null;
-
-  for (const player of players) {
-    const selectedSquareIds = state.currentGameMoves
-      .filter((move) => move.player.id === player.id)
-      .map((move) => move.squareId);
-
-    for (const pattern of winningPatterns) {
-      if (pattern.every((v) => selectedSquareIds.includes(v))) {
-        winner = player;
-      }
-    }
-  }
-
-  return {
-    moves: state.currentGameMoves,
-    currentPlayer,
-    status: {
-      isComplete: winner != null || state.currentGameMoves.length === 9,
-      winner,
-    },
-  };
-}
-
-function deriveStats(state: GameState) {
-  return {
-    playerWithStats: players.map((player) => {
-      const wins = state.history.currentRoundGames.filter(
-        (game) => game.status.winner?.id === player.id
-      ).length;
-
-      return {
-        ...player,
-        wins,
-      };
-    }),
-    ties: state.history.currentRoundGames.filter(
-      (game) => game.status.winner === null
-    ).length,
-  };
-}
+import { deriveGame, deriveStats } from "./utils";
 
 export default function App() {
   const [state, setState] = useLocalStorage<GameState>("game-state-key", {

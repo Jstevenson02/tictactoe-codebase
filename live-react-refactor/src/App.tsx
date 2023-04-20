7eb346a1cbfd8f7aa425076b92ca0d3acedb3197
jstevenson02 <jacobryanstevenson@gmail.com>
2023-04-20 14:58:43 -0500

    Menu scores, Modal triggers, deriveGame and state

--- live-react-refactor/src/App.tsx
+++ live-react-refactor/src/App.tsx
@@ -1,9 +1,95 @@
+import { useState } from "react";
 import "./App.css";
 import Footer from "./components/Footer";
+import Menu from "./components/Menu";
 import Modal from "./components/Modal";
+import { GameState, Player } from "./types";
+import classNames from "classnames";
+
+const players: Player[] = [
+  {
+    id: 1,
+    name: "Player 1",
+    iconClass: "fa-x",
+    colorClass: "yellow",
+  },
+  {
+    id: 2,
+    name: "Player 2",
+    iconClass: "fa-o",
+    colorClass: "turquoise",
+  },
+];
+
+function deriveGame(state: GameState) {
+  const currentPlayer = players[state.currentGameMoves.length % 2];
+
+  const winningPatterns = [
+    [1, 2, 3],
+    [1, 5, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 5, 7],
+    [3, 6, 9],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+
+  let winner = null;
+
+  for (const player of players) {
+    const selectedSquareIds = state.currentGameMoves
+      .filter((move) => move.player.id === player.id)
+      .map((move) => move.squareId);
+
+    for (const pattern of winningPatterns) {
+      if (pattern.every((v) => selectedSquareIds.includes(v))) {
+        winner = player;
+      }
+    }
+  }
+
+  return {
+    moves: state.currentGameMoves,
+    currentPlayer,
+    status: {
+      isComplete: winner != null || state.currentGameMoves.length === 9,
+      winner,
+    },
+  };
+}
+
+function deriveStats(state: GameState) {
+  return {
+    playerWithStats: players.map((player) => {
+      const wins = state.history.currentRoundGames.filter(
+        (game) => game.status.winner?.id === player.id
+      ).length;
+
+      return {
+        ...player,
+        wins,
+      };
+    }),
+    ties: state.history.currentRoundGames.filter(
+      (game) => game.status.winner === null
+    ).length,
+  };
+}
 
 export default function App() {
-  const showModal = false;
+  const [state, setState] = useState({
+    currentGameMoves: [],
+    history: {
+      currentRoundGames: [],
+      allGames: [],
+    },
+  });
+
+  const game = deriveGame(state);
+  const stats = deriveStats(state);
+
+  function handlePlayerMove(squareId: number, player: Player) {}
 
   return (
     <>
@@ -14,52 +100,69 @@ export default function App() {
             <p className="yellow">Player 1, you're up!</p>
           </div>
 
-          <div className="menu" data-id="menu">
-            <button className="menu-btn" data-id="menu-btn">
-              Actions
-              <i className="fa-solid fa-chevron-down"></i>
-            </button>
-            <div className="items border hidden" data-id="menu-items">
-              <button data-id="reset-btn">Reset</button>
-              <button data-id="new-round-btn">New Round</button>
-            </div>
-          </div>
+          <Menu
+            onNewRound={() => console.log()}
+            onReset={() => console.log()}
+          />
 
-          <div id="1" className="square shadow" data-id="square"></div>
-          <div id="2" className="square shadow" data-id="square"></div>
-          <div id="3" className="square shadow" data-id="square"></div>
-          <div id="4" className="square shadow" data-id="square"></div>
-          <div id="5" className="square shadow" data-id="square"></div>
-          <div id="6" className="square shadow" data-id="square"></div>
-          <div id="7" className="square shadow" data-id="square"></div>
-          <div id="8" className="square shadow" data-id="square"></div>
-          <div id="9" className="square shadow" data-id="square"></div>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((squareId) => {
+            const existingMove = game.moves.find(
+              (move) => move.squareId === squareId
+            );
+
+            return (
+              <div
+                key={squareId}
+                className="square shadow"
+                onClick={() => {
+                  if (existingMove) return;
+                  handlePlayerMove(squareId, game.currentPlayer);
+                }}
+              >
+                {existingMove && (
+                  <i
+                    className={classNames(
+                      "fa-solid",
+                      existingMove.player.colorClass,
+                      existingMove.player.iconClass
+                    )}
+                  ></i>
+                )}
+              </div>
+            );
+          })}
 
           <div
             className="score shadow"
             style={{ backgroundColor: "var(--yellow)" }}
           >
             <p>Player 1</p>
-            <span data-id="p1-wins">0 Wins</span>
+            <span>{stats.playerWithStats[0].wins} wins</span>
           </div>
           <div
             className="score shadow"
             style={{ backgroundColor: "var(--light-gray)" }}
           >
             <p>Ties</p>
-            <span data-id="ties">0</span>
+            <span>{stats.ties}</span>
           </div>
           <div
             className="score shadow"
             style={{ backgroundColor: "var(--turquoise)" }}
           >
             <p>Player 2</p>
-            <span data-id="p2-wins">0 Wins</span>
+            <span>{stats.playerWithStats[1].wins} wins</span>
           </div>
         </div>
       </main>
       <Footer />
-      {showModal && <Modal message="Player 1 wins!" />}
+      {game.status.isComplete && (
+        <Modal
+          message={
+            game.status.winner ? `${game.status.winner.name} wins!` : "Tie!"
+          }
+        />
+      )}
     </>
   );
 }

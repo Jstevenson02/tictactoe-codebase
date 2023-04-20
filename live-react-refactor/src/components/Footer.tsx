9a64dcbe8538bab26a4145191dc2f84ea101bdf2
jstevenson02 <jacobryanstevenson@gmail.com>
2023-04-20 14:02:43 -0500

    Footer refactored to react, Modal refactored

---
+++ live-react-refactor/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Created by <a href="https://github.com/Jstevenson02">Jacob Stevenson</a>
+      </p>
+      <p>Copyright © 2023</p>
+    </footer>
+  );
+}
